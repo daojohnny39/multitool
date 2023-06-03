ad8099a1489c618027c02d13f73b413f21b7cce7
Nhat Dao <ndao1@stumail.jccc.edu>
2023-06-03 16:33:58 -0500

    Small Changes - added description

--- ValorantAgents/agentsjs.js
+++ ValorantAgents/agentsjs.js
@@ -4,9 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(response => {return response.json()})
     .then (data => {
         //console.log(data);
-        const agent = data.data[0];
+        const agent = data.data[4];
         const name = agent.displayName;
+        const desc = agent.description;
         document.querySelector('header').innerHTML = name;
+        document.querySelector('h1').innerHTML = desc;
     })
     .catch(error => {
         console.log('Error', error);
